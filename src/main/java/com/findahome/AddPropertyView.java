27f22f39da9d86e927ba924792b7b48f96c48052
amoney79 <mackenziejoseph396@gmail.com>
2026-02-02 19:18:15 +0300

    updates

--- src/main/java/com/findahome/AddPropertyView.java
+++ src/main/java/com/findahome/AddPropertyView.java
@@ -8,7 +8,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
