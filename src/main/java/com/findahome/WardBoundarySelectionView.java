eda6e261e1c1caf448855738d1bf325a6c402585
amoney79 <mackenziejoseph396@gmail.com>
2026-02-02 19:36:18 +0300

    updates

--- src/main/java/com/findahome/WardBoundarySelectionView.java
+++ src/main/java/com/findahome/WardBoundarySelectionView.java
@@ -7,9 +7,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
